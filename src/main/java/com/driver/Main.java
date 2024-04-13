33dd46e27da909e30be927f338f1781967e34e13
VishalSinghShekhawat <vss1664883@gmail.com>
2024-04-13 12:13:32 +0530

    Attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,25 @@ package com.driver;
 public class Main {
 
     public static class Product{
-        public int product(int x,int y){
-            return 0;
+        public void product(int x,int y){
+
         }
 
-        public int product(int x,int y,int z){
-            return 0;
+        public void product(int x,int y,int z){
+
         }
 
-        public double product(double x,double y){
-            return 0;
+        public void product(double x,double y){
+
         }
     }
 
     public static void main(String[] args){
         Product p = new Product();
         p.product(3,4);
+
         p.product(2,3,4);
+
         p.product(2.5,3.5);
     }
 }
